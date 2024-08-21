2afe15aa5d538fa30c996f1376325aefa9c30f00
rylgyl <79947649+rylgyl@users.noreply.github.com>
2024-08-20 23:24:03 -0400

    splash and home screens

--- greenlight/babel.config.js
+++ greenlight/babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: [
+    'module:@react-native/babel-preset', // Preset for React Native
+    '@babel/preset-react', // Preset for JSX and React
+  ],
 };
